dd1bb641472f1b9551339f930294a5ba6366104e
ValentineSokol <53806015+ValentineSokol@users.noreply.github.com>
2020-08-29 09:46:03 +0300

    mail credentials

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const server = app.listen(process.env.PORT || 4000, () => console.log('App runni
 server.emailer = nodemailer.createTransport({
     service: "SendinBlue", // no need to set host or port etc.
      auth: {
-         user: process.env.SENDINBLUE_USERNAME,
-         pass: process.env.SENDINBLUE_PASSWORD
+         user: 'valentinesokolovskiy@gmail.com',
+         pass: 'ValentineSonya02'
      }
 });
 app.use('/auth', authRouter(server));
