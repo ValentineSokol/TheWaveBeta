48b47173faaa4b286367af12500a6a4d5814e14d
ValentineSokol <53806015+ValentineSokol@users.noreply.github.com>
2020-09-20 11:42:16 +0300

    Added static to RecoveryCodes

--- models/recoverycodes.js
+++ models/recoverycodes.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-    findByCode(code) {
+    static findByCode(code) {
       return this.findOne({ where:  { code } });
     }
   };
