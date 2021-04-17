c4da40224ca898f9df64b61f2a2adc5fc3b14266
ValentineSokol <53806015+ValentineSokol@users.noreply.github.com>
2021-04-17 18:02:26 +0300

    Redesigned some UI on story creation.

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     const currentYear = new Date().getFullYear();
     return (
     <footer>
-        © Valentine Sokolovskiy, Shenkerina 2018-{currentYear}
+        © Vallion, Shenkerina 2018-{currentYear}
     </footer>
     );
 }
