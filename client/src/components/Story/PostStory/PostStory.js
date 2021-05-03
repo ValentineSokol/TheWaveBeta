0ddc982810f29439480e37756754816747477ff0
ValentineSokol <53806015+ValentineSokol@users.noreply.github.com>
2021-05-03 10:55:27 +0300

    Redesign

--- client/src/components/Story/PostStory/PostStory.js
+++ client/src/components/Story/PostStory/PostStory.js
@@ -7,7 +7,9 @@ import TextArea from "../../reusable/UIKit/Forms/Inputs/TextArea/TextArea";
 import './PostStory.scss';
 import Heading from "../../reusable/UIKit/Headings/Heading/Heading";
 import Button from "../../reusable/UIKit/Forms/Button";
+import ItemGrid from "../../reusable/UIKit/Layout/ItemGrid/ItemGrid";
 
+import storyConstants from "../../../consts/Story/storyConstants";
 
   const PostStory  = () => {
       const [step, setStep] = useState(0);
@@ -18,19 +20,60 @@ import Button from "../../reusable/UIKit/Forms/Button";
           <>
               <Heading>Start writing right away!</Heading>
               <Card>
-                  <LabeledInput label='Chapter Name: ' />
-                  <TextArea label={'Chapter Text:'} />
-                  <Button clickHandler={onChapterWritten}>Next</Button>
+                  <section className='StoryDetails'>
+                    <LabeledInput label='Story Name:' placeholder='Romeo & Juliet' />
+                  </section>
+                  <section>
+                    <LabeledInput label='Chapter Name: ' />
+                    <TextArea label={'Chapter Text:'} />
+                  </section>
+                  <section className='StoryControlButtons'>
+                    <Button>Publish</Button>
+                    <Button clickHandler={onChapterWritten}>Advanced</Button>
+                  </section>
               </Card>
           </>;
 
       const formStepTwo =
           <>
-              <Heading>Now, let's add some info about your story.</Heading>
-              <Card>
-                  <LabeledInput label='Story Name:' placeholder='Romeo & Juliet' />
-                  <ExtendableContent label='Advanced Settings' tooltip='These are more specific fields, which would help our readers find a story to their liking. Not mandatory. '>
-                    <p>I am a setting!</p>
+              <Heading>Advanced Settings.</Heading>
+              <Card className='AdvancedSettingsCard'>
+                  <ExtendableContent label='Cowriters' tooltip='Manage your cowriters and their permissions.'>
+                      {storyConstants.COWRITER_ROLES.map(role => <p style={{ color: role.color}}>{role.name}</p>)}
+                      <a href='#'>Add</a>
+                  </ExtendableContent>
+                  <ExtendableContent label='Maturity' tooltip='Here you can choose adult themes, depicted in your story. Our system will assign an appropriate maturity rating, based on your choices. '>
+                    <ItemGrid>
+                    <label>Nudity and sex</label>
+                    <input type='checkbox' />
+                    <label>LGBT</label>
+                    <input type='checkbox' />
+                    <label>Violence and gore</label>
+                    <input type='checkbox' />
+                    <label>Alcohol and drug use</label>
+                    <input type='checkbox' />
+                    <label>Depression</label>
+                    <input type='checkbox' />
+                    <label>Suicide</label>
+                    <input type='checkbox' />
+                    <label>Mental abuse</label>
+                    <input type='checkbox' />
+                    <label>Child abuse</label>
+                    <input type='checkbox' />
+                    <label>Sexual abuse</label>
+                    <input type='checkbox' />
+                    <label>Bad Language</label>
+                    <input type='checkbox' />
+                    </ItemGrid>
+                  </ExtendableContent>
+                  <ExtendableContent label='Fandoms' tooltip='Here you can choose, which fandom characters participate in the story and what are their relationships'>
+                     <ExtendableContent label='Characters'>
+                        <p>Naruto Uzumaki (Naruto)</p>
+                        <p>Sasuke Uchiha (Naruto)</p>
+                        <p>Sakura Haruno (Naruto)</p>
+                        <p>Kakashi Hatake (Naruto)</p>
+                     </ExtendableContent>
+
                   </ExtendableContent>
                   <div style={{ display: 'flex', width: '100%', justifyContent: 'center' } }>
                     <Button clickHandler={() => setStep(0)}>Back</Button>
