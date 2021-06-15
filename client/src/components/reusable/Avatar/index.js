42cfeb2948e562d19338ce670453f5218dc67e54
ValentineSokol <53806015+ValentineSokol@users.noreply.github.com>
2021-06-15 21:02:27 +0300

    Bug fixes with Chat

--- client/src/components/reusable/Avatar/index.js
+++ client/src/components/reusable/Avatar/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import defaultAvatar from '../../../assets/defaultAvatar.webp';
-const imageRef = React.createRef();
-const onError = () => {
-    imageRef.current.src = defaultAvatar;
+
+const Avatar = ({ url = defaultAvatar, alt = 'Avatar image' }) => {
+    const [src, setSrc] = useState(url);
+    return <img onError={() => setSrc(defaultAvatar)} src={src} alt={alt} />;
 }
-const Avatar = ({ url = defaultAvatar, alt = 'Avatar image' }) =>  <img onError={onError} ref={imageRef} src={url} alt={alt} />
 
 export default Avatar;
