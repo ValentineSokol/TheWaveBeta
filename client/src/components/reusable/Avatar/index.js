f219329766ccac1c36e9109dbdf84072aa4877b5
ValentineSokol <53806015+ValentineSokol@users.noreply.github.com>
2021-06-17 09:24:54 +0300

    Avatar fixes.

--- client/src/components/reusable/Avatar/index.js
+++ client/src/components/reusable/Avatar/index.js
@@ -1,9 +1,14 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import defaultAvatar from '../../../assets/defaultAvatar.webp';
 
-const Avatar = ({ url = defaultAvatar, alt = 'Avatar image' }) => {
+const Avatar = ({ url, alt = 'Avatar image' }) => {
     const [src, setSrc] = useState(url);
-    return <img onError={() => setSrc(defaultAvatar)} src={src} alt={alt} />;
+    useEffect(() => {
+        setSrc(url);
+    },
+    [url]
+    );
+    return <img onError={ _ => setSrc(defaultAvatar) } src={src} alt={alt} />;
 }
 
 export default Avatar;
