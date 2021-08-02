16ab436d9035c4e8c9540db6a362c4f7b07e021b
ValentineSokol <53806015+ValentineSokol@users.noreply.github.com>
2021-08-02 09:57:42 +0300

    Specify node version in package.json

--- migrations/20210731113159-create-messages.js
+++ migrations/20210731113159-create-messages.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('messages', {
+    await queryInterface.createTable('Messages', {
       id: {
         allowNull: false,
         autoIncrement: true,
