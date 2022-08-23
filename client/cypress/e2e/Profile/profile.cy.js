90064333344b1557eaf06c69e2f8f47774fe505d
ValentineSokol <53806015+ValentineSokol@users.noreply.github.com>
2022-08-23 14:10:02 +0300

    Added E2E for porn protection.

--- client/cypress/e2e/Profile/profile.cy.js
+++ client/cypress/e2e/Profile/profile.cy.js
@@ -6,18 +6,48 @@ describe('Profile', () => {
                 cy.visit('/');
                 cy.getByTestId('profileBtn').click();
             })
-    })
-    it('should be able to change the avatar image', () => {
-        cy.intercept('PATCH', '/users').as('updateUser');
-        cy.intercept('GET', '/files/*').as('getImage');
-        const avatarImage = cy.getByTestId('ProfileAvatar');
-        avatarImage.click();
-        cy.getByTestId('ChangeAvatarFileInput')
-            .attachFile('avatar.png');
-        cy.get('.cropper-crop-box').move({ deltaX: 80, deltaY: 0});
-        cy.getByTestId('PhotoCropperSubmit').click();
-        cy.wait('@updateUser')
-            .then(() => cy.wait('@getImage'))
-            .then(() => avatarImage.compareSnapshot('correct-avatar'));
     });
+    describe('Change Avatar', () => {
+        beforeEach(() => {
+            cy.intercept('PATCH', '/users').as('updateUser');
+            cy.intercept('GET', '/files/*').as('getImage');
+            const avatarImage = cy.getByTestId('ProfileAvatar');
+            avatarImage.click();
+        });
+        const selectImageForCrop = (filename) => {
+            cy.getByTestId('ChangeAvatarFileInput')
+                .attachFile(filename);
+        };
+        const moveCropBox = (deltaX, deltaY) => {
+            cy.get('.cropper-crop-box').move({ deltaX, deltaY });
+        };
+        const waitForAvatarUpdate = () => {
+           return cy.wait('@updateUser')
+                .then(() => cy.wait('@getImage'))
+        }
+
+        it('should be able to change the avatar image', () => {
+            const avatarImage = cy.getByTestId('ProfileAvatar');
+            selectImageForCrop('avatar.png')
+            moveCropBox(80, 0);
+            cy.getByTestId('PhotoCropperSubmit').click();
+            waitForAvatarUpdate()
+                .then(() => avatarImage.compareSnapshot('correct-avatar'));
+        });
+
+        it('when uploading a NSFW image, it should display with protection', () => {
+            const avatarImage = cy.getByTestId('ProfileAvatar');
+            selectImageForCrop('pornAvatar.jpg')
+            moveCropBox(80, 80);
+            cy.getByTestId('PhotoCropperSubmit').click();
+            waitForAvatarUpdate()
+                .then(() => {
+                    avatarImage.compareSnapshot('nsfw-image-protection');
+                    cy.getByTestId('ProfileAvatar').click();
+                    avatarImage.compareSnapshot('nsfw-image-protection-revealed');
+
+                });
+
+        });
+    })
 });
