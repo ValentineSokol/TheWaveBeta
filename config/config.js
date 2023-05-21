592efe889546381b0f37b8496eecff04414e7f09
ValentineSokol <vallyull98@gmail.com>
2023-05-21 09:19:58 +0300

    add MySQL volume to the docker.

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     host: '127.0.0.1',
     "username": "root",
-    "password": "pass",
+    "password": "1",
     "database": "thewavebeta",
     "dialect": "mysql",
     logging: false,
